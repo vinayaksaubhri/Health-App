d0e2b2074d4a1d3366ec6e39b63064d1e3bd33e4
vinayak <vinayaksaubhri@gmail.com>
2021-05-03 05:17:47 +0530

    Updates

--- src/Screens/Hospitals/UI/HospitalCard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from "./HospitalCard.module.css";
-const HospitalCard = (props)=>{
-    return <div className={styles["hospital-card"]}>
-        {props.children}
-    </div>
-}
-export default HospitalCard;
