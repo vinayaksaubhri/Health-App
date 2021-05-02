3c2339d8497f29627c527fd07fbb2e1aaa1911eb
vinayak <vinayaksaubhri@gmail.com>
2021-05-03 05:05:57 +0530

    adding router

--- src/UI/Components/NavBar.js
+++ src/UI/Components/NavBar.js
@@ -1,26 +1,29 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 import styles from "./NavBar.module.css";
+import home from '../Icons/home.svg';
+import hospital from '../Icons/hospital.svg';
+import Vaccine from '../Icons/vaccine.svg';
 
 const NavBar = () => {
   return (
       <div className={styles["nav-bar"]}>
          <Link to="/">
             <div className={styles["nav-bar__home"]}>
-             <span></span>
-             <div>Home</div>
+             <img src={home} className={styles["nav-bar__home_icon"]}/>
+             <div><text>Home</text></div>
            </div>
           </Link>
           <Link to="/hospital">
         <div className={styles["nav-bar__hospital"]}>
-          <span></span>
-          <div>Hospital</div>
+        <img src={hospital} className={styles["nav-bar__home_icon"]}/>
+          <div><text>Hospital</text></div>
         </div>
         </Link>
         <Link to="/ChatScreen">
         <div className={styles["nav-bar__vaccine"]}>
-          <span></span>
-          <div>Vaccine</div>
+        <img src={Vaccine} className={styles["nav-bar__home_icon"]}/>
+          <div><text>Vaccine</text></div>
         </div>
         </Link>
       </div>
