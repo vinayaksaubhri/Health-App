9ba27bbc56aac325a1a711f520cb5c066e880a43
vinayak <vinayaksaubhri@gmail.com>
2021-05-03 04:26:48 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Status_bar from '../src/UI/Components/Status_bar'
 import ChatScreen from '../src/Screens/ChatScreen/chatscreen'
 import Navbar from '../src/UI/Components/NavBar'
 import Hospitals from '../src/Screens/Hospitals/Hospitals'
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
 const App = () => {
   return (
   <div className={styles['iphone_frame']} >
+     <Router>
      <Status_bar/>
-     {/* <ChatScreen/> */}
-     <Hospitals/>
+     <Route path="/" exact component={ChatScreen}></Route>
+     <Route path="/hospital" exact component={Hospitals}></Route>
+     <Route path="/ChatScreen" exact component={ChatScreen}></Route>
      <Navbar/>
+     </Router>
    </div>
   );
 };
--- src/UI/Components/NavBar.js
+++ src/UI/Components/NavBar.js
@@ -1,22 +1,28 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
       <div className={styles["nav-bar"]}>
-        <div className={styles["nav-bar__home"]}>
-          <span></span>
-          <div>Home</div>
-        </div>
+         <Link to="/">
+            <div className={styles["nav-bar__home"]}>
+             <span></span>
+             <div>Home</div>
+           </div>
+          </Link>
+          <Link to="/hospital">
         <div className={styles["nav-bar__hospital"]}>
           <span></span>
           <div>Hospital</div>
         </div>
+        </Link>
+        <Link to="/ChatScreen">
         <div className={styles["nav-bar__vaccine"]}>
           <span></span>
           <div>Vaccine</div>
         </div>
+        </Link>
       </div>
   );
 };
