bd7a5f6566b473f6cfffe581fbaeb58fbfbba23c
pradumn <pradumnpatel18@gmail.com>
2021-05-02 21:48:50 +0530

    adding vaccination screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Status_bar from './UI/Components/Status_bar'
+import Vaccination from './UI/Components/Vaccination'
 
 const App = ()=>{
   return (
     <>
   <Status_bar/>
+  <Vaccination/>
   </>
   )}
 export default App;
