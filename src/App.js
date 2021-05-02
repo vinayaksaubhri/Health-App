3953f907d42a1e4c14c403611684d091fe96161f
vinayak <vinayaksaubhri@gmail.com>
2021-05-03 04:02:07 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-<<<<<<< HEAD
 import styles from '../src/App.module.css'
 import React from "react";
 import Status_bar from '../src/UI/Components/Status_bar'
 import ChatScreen from '../src/Screens/ChatScreen/chatscreen'
 import Navbar from '../src/UI/Components/NavBar'
 import Hospitals from '../src/Screens/Hospitals/Hospitals'
-import Vaccination from './Screens/Vaccine/Vaccination'
 const App = () => {
   return (
   <div className={styles['iphone_frame']} >
      <Status_bar/>
-     <Vaccination/>
      {/* <ChatScreen/> */}
-     {/* <Hospitals/> */}
+     <Hospitals/>
      <Navbar/>
    </div>
   );
 };
 export default App;
-=======
-import React from 'react';
-import Status_bar from './UI/Components/Status_bar'
-import Vaccination from './UI/Components/Vaccination'
-
-const App = ()=>{
-  return (
-    <>
-  <Status_bar/>
-  <Vaccination/>
-  </>
-  )}
-export default App;
->>>>>>> origin/master
