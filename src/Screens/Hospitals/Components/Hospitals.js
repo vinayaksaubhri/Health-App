b14d27c629c15155126d0079bc9d939ec284a340
Akshay Dohroo <akshay.dohroo3@gmail.com>
2021-05-02 17:58:16 +0530

    break

--- src/Screens/Hospitals/Components/Hospitals.js
+++ src/Screens/Hospitals/Components/Hospitals.js
@@ -6,6 +6,13 @@ import Hospital from "./Hospital";
 import styles from "./Hospitals.module.css";
 const data = [
   {
+    hospital_name: "Batra Hospital",
+    hospital_tel: "8376904128",
+    hospital_loc: "New Delhi",
+    isOxygen: true,
+    hospital_beds: "30",
+  },
+  {
     hospital_name: "Action Hospital",
     hospital_tel: "8376904128",
     hospital_loc: "New Delhi",
@@ -13,7 +20,7 @@ const data = [
     hospital_beds: "30",
   },
   {
-    hospital_name: "Batra Hospital",
+    hospital_name: "Action Hospital",
     hospital_tel: "8376904128",
     hospital_loc: "New Delhi",
     isOxygen: true,
@@ -58,15 +65,17 @@ const data = [
 
 const Hospitals = () => {
   return (
-    <div>
+    <div className={styles["hospitals"]}>
       <SearchBar></SearchBar>
-      <div className={styles['hospital-section']}> 
+      <div className={styles["hospital-section"]}>
         {data.map((hospital) => {
           return (
             <Hospital
               hospitalName={hospital.hospital_name}
               hospitalPhone={hospital.hospital_tel}
               hospitalLoc={hospital.hospital_loc}
+              isOxygen = {hospital.isOxygen}
+              hospital_beds = {hospital.hospital_beds}
             />
           );
         })}
